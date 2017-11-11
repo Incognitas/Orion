654a027138d05d617930b06cee40f21691b172d5
yhql <yhql@users.noreply.github.com>
2017-11-11 23:01:56 +0100

    -opcode_

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -23,7 +23,7 @@ pub fn interpreter(execution_context: &mut Context) -> Result<(), InterpreterErr
         //println!("Found bytecode : {:02X}", current_opcode.unwrap() as u8);
         match current_opcode {
             // bytecode 0 : NOP
-            bytecode::opcode_nop => {
+            bytecode::nop => {
                 println!(
                     "NOP bytecode reached at offset 0x{:X}",
                     execution_context.bytecode_fetcher.current_offset()
@@ -31,7 +31,7 @@ pub fn interpreter(execution_context: &mut Context) -> Result<(), InterpreterErr
                 panic!("Unexpected NOP bytecode !");
             }
             // bytecode 1 : ACONST_NULL
-            bytecode::opcode_aconst_null => {
+            bytecode::aconst_null => {
                 try!(
                     execution_context
                         .variables_stack
@@ -40,58 +40,58 @@ pub fn interpreter(execution_context: &mut Context) -> Result<(), InterpreterErr
             }
 
             // bytecode 2 : SCONST_M1
-            bytecode::opcode_sconst_m1 => {
+            bytecode::sconst_m1 => {
                 execution_context.variables_stack.spush(-1 as i16)?;
             }
 
             // bytecode 3: SCONST_0
-            bytecode::opcode_sconst_0 => execution_context.variables_stack.spush(0)?,
+            bytecode::sconst_0 => execution_context.variables_stack.spush(0)?,
             // bytecode 4: SCONST_1
-            bytecode::opcode_sconst_1 => execution_context.variables_stack.spush(1)?,
+            bytecode::sconst_1 => execution_context.variables_stack.spush(1)?,
             // bytecode 5: SCONST_2
-            bytecode::opcode_sconst_2 => execution_context.variables_stack.spush(2)?,
+            bytecode::sconst_2 => execution_context.variables_stack.spush(2)?,
             // bytecode 6: SCONST_3
-            bytecode::opcode_sconst_3 => execution_context.variables_stack.spush(3)?,
+            bytecode::sconst_3 => execution_context.variables_stack.spush(3)?,
             // bytecode 7: SCONST_4
-            bytecode::opcode_sconst_4 => execution_context.variables_stack.spush(4)?,
+            bytecode::sconst_4 => execution_context.variables_stack.spush(4)?,
             // bytecode 8: SCONST_5
-            bytecode::opcode_sconst_5 => execution_context.variables_stack.spush(5)?,
+            bytecode::sconst_5 => execution_context.variables_stack.spush(5)?,
             // bytecode 9: ICONST_M1
-            bytecode::opcode_iconst_m1 => execution_context.variables_stack.ipush(-1)?,
+            bytecode::iconst_m1 => execution_context.variables_stack.ipush(-1)?,
             // bytecode 10: ICONST_0
-            bytecode::opcode_iconst_0 => execution_context.variables_stack.ipush(0)?,
+            bytecode::iconst_0 => execution_context.variables_stack.ipush(0)?,
             // bytecode 11: ICONST_1
-            bytecode::opcode_iconst_1 => execution_context.variables_stack.ipush(1)?,
+            bytecode::iconst_1 => execution_context.variables_stack.ipush(1)?,
             // bytecode 12: ICONST_2
-            bytecode::opcode_iconst_2 => execution_context.variables_stack.ipush(2)?,
+            bytecode::iconst_2 => execution_context.variables_stack.ipush(2)?,
             // bytecode 13: ICONST_3
-            bytecode::opcode_iconst_3 => execution_context.variables_stack.ipush(3)?,
+            bytecode::iconst_3 => execution_context.variables_stack.ipush(3)?,
             // bytecode 14: ICONST_4
-            bytecode::opcode_iconst_4 => execution_context.variables_stack.ipush(4)?,
+            bytecode::iconst_4 => execution_context.variables_stack.ipush(4)?,
             // bytecode 15: ICONST_5
-            bytecode::opcode_iconst_5 => execution_context.variables_stack.ipush(5)?,
+            bytecode::iconst_5 => execution_context.variables_stack.ipush(5)?,
             // bytecode 16: BSPUSH
-            bytecode::opcode_bspush => execution_context
+            bytecode::bspush => execution_context
                 .variables_stack
                 .spush(execution_context.bytecode_fetcher.fetch_b()? as i16)?,
             // bytecode 17: SSPUSH
-            bytecode::opcode_sspush => execution_context
+            bytecode::sspush => execution_context
                 .variables_stack
                 .spush(execution_context.bytecode_fetcher.fetch_s()? as i16)?,
             // bytecode 18: BIPUSH
-            bytecode::opcode_bipush => execution_context
+            bytecode::bipush => execution_context
                 .variables_stack
                 .ipush(execution_context.bytecode_fetcher.fetch_b()? as i32)?,
             // bytecode 19: SIPUSH
-            bytecode::opcode_sipush => execution_context
+            bytecode::sipush => execution_context
                 .variables_stack
                 .ipush(execution_context.bytecode_fetcher.fetch_s()? as i32)?,
             // bytecode 20: IIPUSH
-            bytecode::opcode_iipush => execution_context
+            bytecode::iipush => execution_context
                 .variables_stack
                 .ipush(execution_context.bytecode_fetcher.fetch_i()? as i32)?,
             // bytecode 21: ALOAD
-            bytecode::opcode_aload => {
+            bytecode::aload => {
                 let index = execution_context.bytecode_fetcher.fetch_b()?;
                 // read local from current frame
                 let current_local = execution_context
@@ -102,7 +102,7 @@ pub fn interpreter(execution_context: &mut Context) -> Result<(), InterpreterErr
             }
 
             // bytecode 22: SLOAD
-            bytecode::opcode_sload => {
+            bytecode::sload => {
                 let index = execution_context.bytecode_fetcher.fetch_b()?;
                 // read local from current frame
                 let current_local = execution_context
@@ -113,7 +113,7 @@ pub fn interpreter(execution_context: &mut Context) -> Result<(), InterpreterErr
             }
 
             // bytecode 23: ILOAD
-            bytecode::opcode_iload => {
+            bytecode::iload => {
                 let index = execution_context.bytecode_fetcher.fetch_b()?;
                 // note: the int takes 2 slots in the VM
                 // we have to read those 2 variables and put them on the stack
@@ -131,13 +131,13 @@ pub fn interpreter(execution_context: &mut Context) -> Result<(), InterpreterErr
             }
 
             // bytecode 24...27: ALOAD_0...ALOAD_3
-            bytecode::opcode_aload_0 |
-            bytecode::opcode_aload_1 |
-            bytecode::opcode_aload_2 |
-            bytecode::opcode_aload_3 => {
+            bytecode::aload_0 |
+            bytecode::aload_1 |
+            bytecode::aload_2 |
+            bytecode::aload_3 => {
                 // read local from current frame
                 let current_local = execution_context.current_frame()?.get_local_check_type(
-                    current_opcode as u8 - bytecode::opcode_aload_0 as u8,
+                    current_opcode as u8 - bytecode::aload_0 as u8,
                     StackEntryType::reference,
                 )?;
 
@@ -145,13 +145,13 @@ pub fn interpreter(execution_context: &mut Context) -> Result<(), InterpreterErr
             }
 
             // bytecode 28...31: SLOAD_0...SLOAD_3
-            bytecode::opcode_sload_0 |
-            bytecode::opcode_sload_1 |
-            bytecode::opcode_sload_2 |
-            bytecode::opcode_sload_3 => {
+            bytecode::sload_0 |
+            bytecode::sload_1 |
+            bytecode::sload_2 |
+            bytecode::sload_3 => {
                 // read local from current frame
                 let current_local = execution_context.current_frame()?.get_local_check_type(
-                    current_opcode as u8 - bytecode::opcode_aload_0 as u8,
+                    current_opcode as u8 - bytecode::aload_0 as u8,
                     StackEntryType::short,
                 )?;
 
@@ -159,26 +159,26 @@ pub fn interpreter(execution_context: &mut Context) -> Result<(), InterpreterErr
             }
 
             // bytecode 32...35: ILOAD_0...ILOAD_3
-            bytecode::opcode_iload_0 |
-            bytecode::opcode_iload_1 |
-            bytecode::opcode_iload_2 |
-            bytecode::opcode_iload_3 => {
+            bytecode::iload_0 |
+            bytecode::iload_1 |
+            bytecode::iload_2 |
+            bytecode::iload_3 => {
                 // read local from current frame
-                let current_opcode_val = current_opcode as u8 - bytecode::opcode_iload_0 as u8;
+                let current_val = current_opcode as u8 - bytecode::iload_0 as u8;
                 let mut current_local = execution_context
                     .current_frame()?
-                    .get_local_check_type(current_opcode_val, StackEntryType::int)?;
+                    .get_local_check_type(current_val, StackEntryType::int)?;
 
                 execution_context.variables_stack.push(current_local)?;
 
                 current_local = execution_context
                     .current_frame()?
-                    .get_local_check_type(current_opcode_val + 1, StackEntryType::int)?;
+                    .get_local_check_type(current_val + 1, StackEntryType::int)?;
 
                 execution_context.variables_stack.push(current_local)?;
             }
 
-            bytecode::opcode_aaload => {
+            bytecode::aaload => {
                 let arrayref = execution_context.variables_stack.pop()?;
                 let index = execution_context.variables_stack.pop()?;
 
--- tests/opcode_test.rs
+++ tests/opcode_test.rs
@@ -6,7 +6,7 @@ use interpreterlib::{bytecodes,interpreter,context,stack,constants};
 #[test]
 #[should_panic]
 fn nop_test() {
-    let opcodes: Vec<u8> = vec![bytecodes::bytecode::opcode_nop as u8];
+    let opcodes: Vec<u8> = vec![bytecodes::bytecode::nop as u8];
     let mut ctx = context::Context::new();
     ctx.bytecode_fetcher.bc_array = opcodes;
     // silence result as we don't care here
@@ -17,7 +17,7 @@ fn nop_test() {
 #[test]
 /// make sure the aconst_null pushes a null reference on the stack
 fn opcode_aconst_null_test() {
-    let opcodes: Vec<u8> = vec![bytecodes::bytecode::opcode_aconst_null as u8];
+    let opcodes: Vec<u8> = vec![bytecodes::bytecode::aconst_null as u8];
     let mut ctx = context::Context::new();
     ctx.bytecode_fetcher.bc_array = opcodes;
     // silence result as we don't care here
