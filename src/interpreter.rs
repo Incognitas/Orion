851a561ff1db83127af16f33ef5b117bbfc86ae6
yhql <yhql@users.noreply.github.com>
2017-11-12 17:29:24 +0100

    sadd

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,6 +1,7 @@
 use bytecodes::bytecode;
 use context::Context;
 use stack::StackEntryType;
+use stack::StackEntry;
 use jcvmerrors::InterpreterError;
 use constants;
 use exceptions;
@@ -211,6 +212,159 @@ pub fn interpreter(execution_context: &mut Context) -> Result<(), InterpreterErr
 
                 // TODO: implement handle table ? heap ?
             }
+            //bytecode::baload,          // 37
+            //bytecode::saload,          // 38
+            //bytecode::iaload,          // 39
+            //bytecode::astore,          // 40
+            //bytecode::sstore,          // 41
+            //bytecode::istore,          // 42
+            //bytecode::astore_0,        // 43
+            //bytecode::astore_1,        // 44
+            //bytecode::astore_2,        // 45
+            //bytecode::astore_3,        // 46
+            //bytecode::sstore_0,        // 47
+            //bytecode::sstore_1,        // 48
+            //bytecode::sstore_2,        // 49
+            //bytecode::sstore_3,        // 50
+            //bytecode::istore_0,        // 51
+            //bytecode::istore_1,        // 52
+            //bytecode::istore_2,        // 53
+            //bytecode::istore_3,        // 54
+            //bytecode::aastore,         // 55
+            //bytecode::bastore,         // 56
+            //bytecode::sastore,         // 57
+            //bytecode::iastore,         // 58
+            //bytecode::pop,             // 59
+            //bytecode::pop2,            // 60
+            //bytecode::dup,             // 61
+            //bytecode::dup2,            // 62
+            //bytecode::dup_x,           // 63
+            //bytecode::swap_x,          // 64
+            bytecode::sadd => {
+                let value1 = execution_context.variables_stack.pop()?;
+                let value2 = execution_context.variables_stack.pop()?;
+                let res = value1.value + value2.value; 
+                execution_context.variables_stack.push(StackEntry::from_values(res,StackEntryType::Short))?;
+            }
+            // bytecode::iadd,            // 66
+            // bytecode::ssub,            // 67
+            // bytecode::isub,            // 68
+            // bytecode::smul,            // 69
+            // bytecode::imul,            // 70
+            // bytecode::sdiv,            // 71
+            // bytecode::idiv,            // 72
+            // bytecode::srem,            // 73
+            // bytecode::irem,            // 74
+            // bytecode::sneg,            // 75
+            // bytecode::ineg,            // 76
+            // bytecode::sshl,            // 77
+            // bytecode::ishl,            // 78
+            // bytecode::sshr,            // 79
+            // bytecode::ishr,            // 80
+            // bytecode::sushr,           // 81
+            // bytecode::iushr,           // 82
+            // bytecode::sand,            // 83
+            // bytecode::iand,            // 84
+            // bytecode::sor,             // 85
+            // bytecode::ior,             // 86
+            // bytecode::sxor,            // 87
+            // bytecode::ixor,            // 88
+            // bytecode::sinc,            // 89
+            // bytecode::iinc,            // 90
+            // bytecode::s2b,             // 91
+            // bytecode::s2i,             // 92
+            // bytecode::i2b,             // 93
+            // bytecode::i2s,             // 94
+            // bytecode::icmp,            // 95
+            // bytecode::ifeq,            // 96
+            // bytecode::ifne,            // 97
+            // bytecode::iflt,            // 98
+            // bytecode::ifge,            // 99
+            // bytecode::ifgt,            // 100
+            // bytecode::ifle,            // 101
+            // bytecode::ifnull,          // 102
+            // bytecode::ifnonnull,       // 103
+            // bytecode::if_acmpeq,       // 104
+            // bytecode::if_acmpne,       // 105
+            // bytecode::if_scmpeq,       // 106
+            // bytecode::if_scmpne,       // 107
+            // bytecode::if_scmplt,       // 108
+            // bytecode::if_scmpge,       // 109
+            // bytecode::if_scmpgt,       // 110
+            // bytecode::if_scmple,       // 111
+            // bytecode::goto,            // 112
+            // bytecode::jsr,             // 113
+            // bytecode::ret,             // 114
+            // bytecode::stableswitch,    // 115
+            // bytecode::itableswitch,    // 116
+            // bytecode::slookupswitch,   // 117
+            // bytecode::ilookupswitch,   // 118
+            // bytecode::areturn,         // 119
+            // bytecode::sreturn,         // 120
+            // bytecode::ireturn,         // 121
+            // bytecode::return_,         // 122
+            // bytecode::getstatic_a,     // 123
+            // bytecode::getstatic_b,     // 124
+            // bytecode::getstatic_s,     // 125
+            // bytecode::getstatic_i,     // 126
+            // bytecode::putstatic_a,     // 127
+            // bytecode::putstatic_b,     // 128
+            // bytecode::putstatic_s,     // 129
+            // bytecode::putstatic_i,     // 130
+            // bytecode::getfield_a,      // 131
+            // bytecode::getfield_b,      // 132
+            // bytecode::getfield_s,      // 133
+            // bytecode::getfield_i,      // 134
+            // bytecode::putfield_a,      // 135
+            // bytecode::putfield_b,      // 136
+            // bytecode::putfield_s,      // 137
+            // bytecode::putfield_i,      // 138
+            // bytecode::invokevirtual,   // 139
+            // bytecode::invokespecial,   // 140
+            // bytecode::invokestatic,    // 141
+            // bytecode::invokeinterface, // 142
+            // bytecode::new,             // 143
+            // bytecode::newarray,        // 144
+            // bytecode::anewarray,       // 145
+            // bytecode::arraylength,     // 146
+            // bytecode::athrow,          // 147
+            // bytecode::checkcast,       // 148
+            // bytecode::instanceof,      // 149
+            // bytecode::sinc_w,          // 150
+            // bytecode::iinc_w,          // 151
+            // bytecode::ifeq_w,          // 152
+            // bytecode::ifne_w,          // 153
+            // bytecode::iflt_w,          // 154
+            // bytecode::ifge_w,          // 155
+            // bytecode::ifgt_w,          // 156
+            // bytecode::ifle_w,          // 157
+            // bytecode::ifnull_w,        // 158
+            // bytecode::ifnonnull_w,     // 159
+            // bytecode::if_acmpeq_w,     // 160
+            // bytecode::if_acmpne_w,     // 161
+            // bytecode::if_scmpeq_w,     // 162
+            // bytecode::if_scmpne_w,     // 163
+            // bytecode::if_scmplt_w,     // 164
+            // bytecode::if_scmpge_w,     // 165
+            // bytecode::if_scmpgt_w,     // 166
+            // bytecode::if_scmple_w,     // 167
+            // bytecode::goto_w,          // 168
+            // bytecode::getfield_a_w,    // 169
+            // bytecode::getfield_b_w,    // 170
+            // bytecode::getfield_s_w,    // 171
+            // bytecode::getfield_i_w,    // 172
+            // bytecode::getfield_a_this, // 173
+            // bytecode::getfield_b_this, // 174
+            // bytecode::getfield_s_this, // 175
+            // bytecode::getfield_i_this, // 176
+            // bytecode::putfield_a_w,    // 177
+            // bytecode::putfield_b_w,    // 178
+            // bytecode::putfield_s_w,    // 179
+            // bytecode::putfield_i_w,    // 180
+            // bytecode::putfield_a_this, // 181
+            // bytecode::putfield_b_this, // 182
+            // bytecode::putfield_s_this, // 183
+            // bytecode::putfield_i_this, // 184
 
             _ => break,
         }
